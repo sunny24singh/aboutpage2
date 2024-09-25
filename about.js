e6bf2bb4cf2f9a1a7a786e08e9711027615179ca
sunny singh <sunny24singh@hotmail.com>
2024-09-26 00:59:56 +0530

    git commit -m "N"

--- about.js
+++ about.js
@@ -40,3 +40,39 @@ drop_close.addEventListener("click" , () => {
 })
 
 
+// section 4 js childs
+ 
+let child1 = document.querySelector("#section4_icon1");
+let child2 = document.querySelector("#section4_icon2");
+let child3 = document.querySelector("#section4_icon3");
+
+let child1_data = document.querySelector("#section4_child1_div");
+
+let child2_data = document.querySelector("#section4_child2_div");
+
+let child3_data = document.querySelector("#section4_child3_div");
+
+//child 1 
+
+
+child1.addEventListener("click" , () => {  
+    
+    child1_data.style.display = "flex"
+    child2_data.style.display = "none"
+    child3_data.style.display = "none"
+})
+
+
+child2.addEventListener("click" , () => {
+    
+    child1_data.style.display = "none"
+    child3_data.style.display = "none"
+    child2_data.style.display = "flex"
+})
+
+child3.addEventListener("click" , () => {
+    
+    child1_data.style.display = "none"
+    child3_data.style.display = "flex"
+    child2_data.style.display = "none"
+})
