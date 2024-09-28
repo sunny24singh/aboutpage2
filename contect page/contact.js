9ea63e5b6ed5d2c0a118c13d89ef65b14b00665e
sunny singh <sunny24singh@hotmail.com>
2024-09-29 00:25:36 +0530

    git commit -m "contact page work"

---
+++ contect page/contact.js
@@ -0,0 +1,40 @@
+
+
+
+
+let icon = document.querySelector("#icon");
+icon.addEventListener("click" , () => {
+    let icon = document.querySelector(".toggt");
+    icon.classList.toggle("toggt2") 
+
+    
+})
+ 
+
+//dropdown js
+
+let drop_btn = document.querySelector("#dropbtn");
+
+let drop_data1 = document.querySelector("#sec");
+
+drop_btn.addEventListener('mouseover' , () => { 
+    drop_data1.style.display = "flex"  
+
+    drop_data1.addEventListener('mouseover' , () => {
+        drop_data1.style.display = "flex"  
+
+
+    })
+})
+
+drop_data1.addEventListener('mouseout' , () => {   
+    drop_data1.style.display = "none"
+})
+
+
+
+let drop_close = document.querySelector("#drop_icon");
+
+drop_close.addEventListener("click" , () => {
+    drop_data1.style.display = "none"
+})
